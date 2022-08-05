e59d6aa1494216b01dd46271063482ec1d306a1d
PriyankaDS2022 <110330365+PriyankaDS2022@users.noreply.github.com>
2022-08-05 14:01:08 +0530

    Add files via upload

---
+++ Untitled14 (1).py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+
+
+# In[2]:
+
+
+os.getcwd()
+
+
+# In[10]:
+
+
+import pandas as pd
+
+
+# In[11]:
+
+
+pd.read_csv("class.tsv",sep="\t")
+
+
+# In[13]:
+
+
+pd.read_csv("https://raw.githubusercontent.com/dsrscientist/dataset1/master/Social_Network_Ads.csv")
+
+
+# In[ ]:
+
+
+
+
